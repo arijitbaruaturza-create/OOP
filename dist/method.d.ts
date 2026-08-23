6f81c667407062317b8496fbe608401eec2eeb23
Clevatess <arijit.barua.turza@g.bracu.ac.bd>
2026-08-23 20:48:03 +0600

    Method in OOP

---
+++ dist/method.d.ts
@@ -0,0 +1,8 @@
+/**
+ * name
+ * email
+ * age
+ * marks
+ */
+export {};
+//# sourceMappingURL=method.d.ts.map
---
+++ src/method.ts
@@ -0,0 +1,35 @@
+/**
+ * name
+ * email
+ * age 
+ * marks
+ */
+
+class Student {
+    name: string
+    id: number
+    age: number
+    email: string
+
+    //constructor ---> A special block of code or method used to initialize an object when it is created.
+    constructor(name: string, id: number, age: number, email: string){
+        this.name = name
+        this.id = id
+        this.age = age
+        this.email = email
+    }
+
+    getInfo(): string{
+        return `Name: ${this.name}\n Email: ${this.email}`
+    }
+}
+
+
+//Instantiate
+//Instance
+const rafi = new Student("Rafi", 22201844, 16, "rafi@gmail.com")
+const sadia = new Student("Sadia", 22201843, 17, "sadia@gmail.com")
+
+console.log(rafi)
+// console.log(sadia)
+console.log(rafi.getInfo())
